bc2dfd64e9c6398db02bdfffbd6a254e173ade88
LukaDudauri7 <lukadudauri01@gmail.com>
2024-03-26 11:26:04 +0400

    draw X or O

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 function App() {
+  const [actvivePlayer, setActivePlayer] = useState('X');
+  function handleSelectSquare() {
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X');
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={actvivePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O" isActive={actvivePlayer === 'O'}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={actvivePlayer}/>
       </div>
     </main>
   )
